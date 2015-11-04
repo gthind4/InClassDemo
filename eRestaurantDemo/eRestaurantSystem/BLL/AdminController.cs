3eec03e39301b7182019cbb58bca704c012493bd
Gurvinderpal Singh Thind <gthind4@nait.ca>
2015-11-03 17:00:16 -0700

    Front desk Updated

--- eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -308,6 +308,49 @@ namespace eRestaurantSystem.BLL
                 return result;
             }
         }
+
+        
+            [DataObjectMethod(DataObjectMethodType.Select)]
+            public List<ReservationCollection> ReservationsByTime(DateTime date)
+            {
+                using (var context = new eRestaurantContext())
+                {
+                    var result = (from data in context.Reservations
+                                  where data.ReservationDate.Year == date.Year
+                                  && data.ReservationDate.Month == date.Month
+                                  && data.ReservationDate.Day == date.Day
+                                      // && data.ReservationDate.Hour == timeSlot.Hours
+                                  && data.ReservationStatus == Reservation.Booked
+                                  select new ReservationSummary()
+                                  {
+                                      ID = data.ReservationID,
+                                      Name = data.CustomerName,
+                                      Date = data.ReservationDate,
+                                      NumberInParty = data.NumberInParty,
+                                      Status = data.ReservationStatus,
+                                      Event = data.Event.Description,
+                                      Contact = data.ContactPhone
+                                  }).ToList();
+
+                    //the second part of this method uses the result of first LINQ query.
+                    //first linq query.
+                    //Linq to Enitity will only execute the query when it deems
+                    //necessary for having the results in memory.
+                    //to get your query to execute and have the resulting data
+                    //inside memory for further use, u can attach the .ToList()
+                    // to the previous query.
+                    var finalResult = from item in result
+                                      orderby item.NumberInParty
+                                      group item by item.Date.Hour into itemGroup
+                                      select new ReservationCollection()
+                                      {
+                                          Hour = itemGroup.Key,
+                                          Reservations = itemGroup.ToList()
+                                      };
+                    return finalResult.OrderBy(x => x.Hour).ToList();
+                }
+            }
+        
         #endregion
     }//eof class
 }//eof namespace
---
+++ eRestaurantDemo/eRestaurantSystem/Entities/DTOs/ReservationCollection.cs
@@ -0,0 +1,22 @@
+﻿using eRestaurantSystem.Entities.POCOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+
+#endregion
+namespace eRestaurantSystem.Entities.DTOs
+{
+    public class ReservationCollection
+    {
+        //data
+        public int Hour { get; set; }
+        public TimeSpan SeatingTime { get { return new TimeSpan(Hour, 0, 0); } }
+
+        //read only property
+        public virtual ICollection<ReservationSummary> Reservations { get; set; }
+    }
+}
